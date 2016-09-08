e93cc8c68459d782bc2a4bb1d626a50e297b606f
amycontreras <user.email>
2016-09-08 15:46:33 -0700

    Arm variables.

--- Macros.h
+++ Macros.h
@@ -5,10 +5,15 @@ int fwd; 		//fwd/rev
 int spin;		//turn in place
 int strafe;
 
+//Arm
+int armOn; //hold to move
 
 void updateOI()
 {
 	fwd = vexRT[Ch3];
 	spin = vexRT[Ch4];
 	strafe = vexRT[Ch2];
+
+	armOn = vexRT[Btn7U];
+
 }
