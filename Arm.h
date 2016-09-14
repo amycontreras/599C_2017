7f72ef2bacc156a63d9ddde2858c69d184d9bae1
amycontreras <amycontreras9@yahoo.com>
2016-09-14 15:57:49 -0700

    Added in arm function that actuates two pistons. Tested and it works.

--- Arm.h
+++ Arm.h
@@ -1,9 +1,21 @@
-#include "Macros.h"
+//#include "Macros.h"
 
 void arm ()
 {
-		while(armOn == true)
+		while(vexRT[Btn7U] == true)
 		{
 			motor[arm_m] = 127;
+		}
+}
+
+void pistons()
+{
+		if(vexRT[Btn7L] == 1)
+		{
+			SensorValue[sol] = 1;
+		}
+		else if(vexRT[Btn7R] == 1)
+		{
+			SensorValue[sol] = 0;
 		}
 }
