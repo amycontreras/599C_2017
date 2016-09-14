e6cc54a32c956d45d4dddf286d2d057717b98216
amycontreras <amycontreras9@yahoo.com>
2016-09-14 14:29:45 -0700

    Removed absolute value and expanded fwd if statement.

--- Drive.h
+++ Drive.h
@@ -2,21 +2,28 @@
 
 void drive()
 {
-	if(abs(fwd) > DEADZONE)
+	if(fwd > DEADZONE)
 	{
 		motor[front_left_drive_m] = fwd;
 		motor[front_right_drive_m] = -fwd;
 		motor[back_left_drive_m] = fwd;
 		motor[back_right_drive_m] = -fwd;
 	}
-	else if(abs(spin) > DEADZONE)
+	else if(fwd < - DEADZONE)
+	{
+		motor[front_left_drive_m] = -fwd;
+		motor[front_right_drive_m] = fwd;
+		motor[back_left_drive_m] = -fwd;
+		motor[back_right_drive_m] = fwd;
+	}
+	else if(spin > DEADZONE)
 	{
 		motor[front_left_drive_m] = spin;
 		motor[front_right_drive_m] = spin;
 		motor[back_left_drive_m] = spin;
 		motor[back_right_drive_m] = spin;
 	}
-	else if(abs(strafe) > DEADZONE)
+	else if(strafe > DEADZONE)
 	{
 		motor[front_left_drive_m] = -strafe;
 		motor[front_right_drive_m] = strafe;
