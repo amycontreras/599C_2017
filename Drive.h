fe044d867b67bdf505fdc93cb35d19a6e886249b
amycontreras <amycontreras9@yahoo.com>
2016-09-26 16:04:54 -0700

    Added an update function for the drive motors. To be tested, not sure if the logic works. Will implement later.

--- Drive.h
+++ Drive.h
@@ -1,5 +1,13 @@
 //#include "Macros.h"
 
+void updateDriveMotors(int front, int back, int left, int right)
+{
+		motor[front_left_drive_m] = front && left;
+		motor[front_right_drive_m] = front && right;
+		motor[back_left_drive_m] = back && left;
+		motor[back_right_drive_m] = back && right;
+}
+
 void drive()
 {
 	if(abs(vexRT[Ch2]) > 15)
