989948fc47d4d1ba50702003b47d2007ea3bc1e7
amycontreras <amycontreras9@yahoo.com>
2016-09-30 13:11:35 -0700

    Updated drive code with variables.

--- Drive.h
+++ Drive.h
@@ -1,35 +1,48 @@
 //#include "Macros.h"
 
+int driveY = 0;
+int driveX = 0;
+int strafe = 0;
+int DEADZONE = 15;
+
+void updateOI()
+{
+	driveY = vexRT[Ch2];
+	driveX = vexRT[Ch4];
+	strafe = vexRT[Ch1];
+}
+
+/*
 void updateDriveMotors(int front, int back, int left, int right)
 {
 		motor[front_left_drive_m] = front && left;
 		motor[front_right_drive_m] = front && right;
 		motor[back_left_drive_m] = back && left;
 		motor[back_right_drive_m] = back && right;
-}
+}*/
 
 void drive()
 {
-	if(abs(vexRT[Ch2]) > 15)
+	if(abs(driveY) > DEADZONE && abs(driveY) > abs(strafe))
 	{
-		motor[front_left_drive_m] = vexRT[Ch2];
-		motor[front_right_drive_m] = -vexRT[Ch2];
-		motor[back_left_drive_m] = -vexRT[Ch2];
-		motor[back_right_drive_m] = vexRT[Ch2];
+		motor[front_left_drive_m] = driveY;
+		motor[front_right_drive_m] = -driveY;
+		motor[back_left_drive_m] = -driveY;
+		motor[back_right_drive_m] = driveY;
 	}
-	else if(abs(vexRT[Ch1]) > 15)
+	else if(abs(driveX) > 15) //TURNING
 	{
-		motor[front_left_drive_m] = vexRT[Ch1];
-		motor[front_right_drive_m] = vexRT[Ch1];
-		motor[back_left_drive_m] = -vexRT[Ch1];
-		motor[back_right_drive_m] = -vexRT[Ch1];
+		motor[front_left_drive_m] = -driveX;
+		motor[front_right_drive_m] = -driveX;
+		motor[back_left_drive_m] = driveX;
+		motor[back_right_drive_m] = driveX;
 	}
-	else if(abs(vexRT[Ch4]) > 15)
+	else if(abs(strafe) > 15)
 	{
-		motor[front_left_drive_m] = vexRT[Ch4];
-		motor[front_right_drive_m] = vexRT[Ch4];
-		motor[back_left_drive_m] = vexRT[Ch4];
-		motor[back_right_drive_m] = vexRT[Ch4];
+		motor[front_left_drive_m] = strafe;
+		motor[front_right_drive_m] = strafe;
+		motor[back_left_drive_m] = strafe;
+		motor[back_right_drive_m] = strafe;
 	}
 	else
 	{
