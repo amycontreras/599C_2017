ee385f487773a8e8733fd74ee9b27147153cabac
amycontreras <amycontreras9@yahoo.com>
2016-09-30 13:12:32 -0700

    Removed pneumatics function, idea was scrapped.

--- Arm.h
+++ Arm.h
@@ -2,20 +2,8 @@
 
 void arm ()
 {
-		while(vexRT[Btn7U] == true)
+		while(vexRT[btn7U] == true)
 		{
 			motor[arm_m] = 127;
-		}
-}
-
-void pistons()
-{
-		if(vexRT[Btn7L] == 1)
-		{
-			SensorValue[sol] = 1;
-		}
-		else if(vexRT[Btn7R] == 1)
-		{
-			SensorValue[sol] = 0;
 		}
 }
