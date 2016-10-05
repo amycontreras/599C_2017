edc6a68889f38c917b3e40b64cb577a3960fbbd1
amycontreras <amycontreras9@yahoo.com>
2016-10-05 13:14:25 -0700

    Added new arm code, all motors run at full speed when you hold down the button.

--- Arm.h
+++ Arm.h
@@ -2,8 +2,18 @@
 
 void arm ()
 {
-		while(vexRT[btn7U] == true)
+		if(vexRT[Btn6U] == 1)
 		{
-			motor[arm_m] = 127;
+			motor[left_back_arm] = 127;
+			motor[left_front_arm] = 127;
+			motor[right_front_arm] = 127;
+			motor[right_back_arm] = 127;
+		}
+		else
+		{
+			motor[left_back_arm] = 0;
+			motor[left_front_arm] = 0;
+			motor[right_front_arm] = 0;
+			motor[right_back_arm] = 0;
 		}
 }
