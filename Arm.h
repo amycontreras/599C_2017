cc6d4be1144b22ecee4591b7095079a0a96eb4fc
amycontreras <amycontreras9@yahoo.com>
2016-10-07 13:02:45 -0700

    Fully commented for the design notebook.

--- Arm.h
+++ Arm.h
@@ -1,3 +1,6 @@
+// This function is essentially a shortcut for our arm code.
+// It sets the motors all to the same speed, so once the function
+// is used elsewhere, all motors move at the same directed speed.
 void updateArmMotors(int speed)
 {
 	motor[left_back_arm] = speed;
@@ -6,6 +9,10 @@ void updateArmMotors(int speed)
 	motor[right_back_arm] = speed;
 }
 
+// This is the actual code for the arm. It checks whether or not
+// button 6U or 6D is pressed on the joystick (6U runs the motors
+// forward, 6D in reverse) and then acts accordingly. If neither
+// buttons are pressed, the arm motors do not move.
 void arm ()
 {
 	if(vexRT[Btn6U] == 1)
--- Drive.h
+++ Drive.h
@@ -1,8 +1,15 @@
+//DEADZONE eliminates human error when driving. It is essentially
+// an area around the joystick, a zone, that does not allow the
+// robot to drive. The joystick must be moved a sufficient amount
+// past the DEADZONE in order for the robot to move.
+int DEADZONE = 15;
+
+// INITIALIZING DRIVE VARIABLES
 int driveY = 0;
 int driveX = 0;
 int strafe = 0;
-int DEADZONE = 15;
 
+// UDPATING DRIVE VARIABLES
 void driveVariables()
 {
 	driveY = vexRT[Ch2];
@@ -10,31 +17,38 @@ void driveVariables()
 	strafe = vexRT[Ch1];
 }
 
-
+// This robot's physical structure requires holonomic drive code.
+// Each motor is specifically modified so it runs in the positive
+// or negative direction. This code checks whether or not the joystick
+// channels are being accessed. By using an absolute value, the code
+// within the 'if' statement is shortened.
 void drive()
 {
-	if(abs(driveY) > DEADZONE && abs(driveY) > abs(strafe))
+	// Notice how driveY must be greater than the strafing joystick to
+	// move. This eliminates one movement overriding the other, and
+	// creates smooth transitions between fwd/rev driving and strafing.
+	if(abs(driveY) > DEADZONE && abs(driveY) > abs(strafe))	// FWD REV
 	{
 		motor[front_left_drive_m] = driveY;
 		motor[front_right_drive_m] = -driveY;
 		motor[back_left_drive_m] = -driveY;
 		motor[back_right_drive_m] = driveY;
 	}
-	else if(abs(driveX) > 15) //TURNING
+	else if(abs(driveX) > 15) 															// TURN
 	{
 		motor[front_left_drive_m] = -driveX;
 		motor[front_right_drive_m] = -driveX;
 		motor[back_left_drive_m] = driveX;
 		motor[back_right_drive_m] = driveX;
 	}
-	else if(abs(strafe) > 15)
+	else if(abs(strafe) > 15)																// STRAFE
 	{
 		motor[front_left_drive_m] = strafe;
 		motor[front_right_drive_m] = strafe;
 		motor[back_left_drive_m] = strafe;
 		motor[back_right_drive_m] = strafe;
 	}
-	else
+	else																										// STOP
 	{
 		motor[front_left_drive_m] = 0;
 		motor[front_right_drive_m] = 0;
