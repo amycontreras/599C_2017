37ded7e57d3870c5f8fcc8bef36e2ce370b36289
amycontreras <amycontreras9@yahoo.com>
2016-10-20 16:01:43 -0700

    Created two autonomous modes, one on the left and one on the right side of the field.

--- Auto.h
+++ Auto.h
@@ -1,13 +1,13 @@
-void autonono()
+void leftAuto()
 {
-	updateArmMotors(-127);												//release arm
-	wait1Msec(3000);
+	updateArmMotors(-70);												//release arm
+	wait1Msec(3500);
 	updateArmMotors(0);													// arm is up, in place
 
-	motor[front_left_drive_m] = 95;				//strafe right 1 sec
-	motor[front_right_drive_m] = 95;
-	motor[back_left_drive_m] = 95;
-	motor[back_right_drive_m] = 95;
+	motor[front_left_drive_m] = -85;				//strafe left	1 sec
+	motor[front_right_drive_m] = -85;
+	motor[back_left_drive_m] = -85;
+	motor[back_right_drive_m] = -85;
 	wait1Msec(1200);
 	motor[front_left_drive_m] = 0;				//stop
 	motor[front_right_drive_m] = 0;
@@ -19,18 +19,18 @@ void autonono()
 	motor[front_right_drive_m] = -95;
 	motor[back_left_drive_m] = -95;
 	motor[back_right_drive_m] = 95;
-	wait1Msec(1250);
+	wait1Msec(2000);
 	motor[front_left_drive_m] = 0;					//stop driving
 	motor[front_right_drive_m] = 0;
 	motor[back_left_drive_m] = 0;
 	motor[back_right_drive_m] = 0;
 
 
-	motor[front_left_drive_m] = 95;				//strafe right
-	motor[front_right_drive_m] = 95;
-	motor[back_left_drive_m] = 95;
-	motor[back_right_drive_m] = 95;
-	wait1Msec(785);
+	motor[front_left_drive_m] = -95;				//strafe left
+	motor[front_right_drive_m] = -95;
+	motor[back_left_drive_m] = -95;
+	motor[back_right_drive_m] = -95;
+	wait1Msec(775);
 	motor[front_left_drive_m] = 0;				//stop
 	motor[front_right_drive_m] = 0;
 	motor[back_left_drive_m] = 0;
@@ -41,7 +41,7 @@ void autonono()
 	motor[front_right_drive_m] = 95;
 	motor[back_left_drive_m] = 95;
 	motor[back_right_drive_m] = -95;
-	wait1Msec(5000);
+	wait1Msec(3000);
 	motor[front_left_drive_m] = 0;					//stop driving
 	motor[front_right_drive_m] = 0;
 	motor[back_left_drive_m] = 0;
@@ -49,6 +49,56 @@ void autonono()
 
 }
 
+
+void rightAuto()
+{
+	updateArmMotors(-70);												//release arm
+	wait1Msec(3500);
+	updateArmMotors(0);													// arm is up, in place
+
+	motor[front_left_drive_m] = 85;				//strafe right	1 sec
+	motor[front_right_drive_m] = 85;
+	motor[back_left_drive_m] = 85;
+	motor[back_right_drive_m] = 85;
+	wait1Msec(1200);
+	motor[front_left_drive_m] = 0;				//stop
+	motor[front_right_drive_m] = 0;
+	motor[back_left_drive_m] = 0;
+	motor[back_right_drive_m] = 0;
+
+
+	motor[front_left_drive_m] = 95;					//drive forward for 1.5 sec
+	motor[front_right_drive_m] = -95;
+	motor[back_left_drive_m] = -95;
+	motor[back_right_drive_m] = 95;
+	wait1Msec(2000);
+	motor[front_left_drive_m] = 0;					//stop driving
+	motor[front_right_drive_m] = 0;
+	motor[back_left_drive_m] = 0;
+	motor[back_right_drive_m] = 0;
+
+
+	motor[front_left_drive_m] = 95;				//strafe right
+	motor[front_right_drive_m] = 95;
+	motor[back_left_drive_m] = 95;
+	motor[back_right_drive_m] = 95;
+	wait1Msec(775);
+	motor[front_left_drive_m] = 0;				//stop
+	motor[front_right_drive_m] = 0;
+	motor[back_left_drive_m] = 0;
+	motor[back_right_drive_m] = 0;
+
+
+	motor[front_left_drive_m] = -95;					//drive rev for 5 sec
+	motor[front_right_drive_m] = 95;
+	motor[back_left_drive_m] = 95;
+	motor[back_right_drive_m] = -95;
+	wait1Msec(3000);
+	motor[front_left_drive_m] = 0;					//stop driving
+	motor[front_right_drive_m] = 0;
+	motor[back_left_drive_m] = 0;
+	motor[back_right_drive_m] = 0;
+}
 // s right
 // fwd
 // s right
