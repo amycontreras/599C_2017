a20b55a12f8ee01cb3b42a45963fe440c0d02a9e
amycontreras <amycontreras9@yahoo.com>
2016-11-19 06:38:26 -0800

    Basically redid everything...condensed code and added several new autos

--- Auto.h
+++ Auto.h
@@ -1,14 +1,43 @@
+void driveReverse(int speed)
+{
+	motor[front_left_drive_m] = -speed;
+	motor[front_right_drive_m] = speed;
+	motor[back_left_drive_m] = speed;
+	motor[back_right_drive_m] = -speed;
+}
+
+void driveForward(int speed)
+{
+	motor[front_left_drive_m] = speed;
+	motor[front_right_drive_m] = -speed;
+	motor[back_left_drive_m] = -speed;
+	motor[back_right_drive_m] = speed;
+}
+
+void strafeRight(int speed)
+{
+	motor[front_left_drive_m] = speed;
+	motor[front_right_drive_m] = speed;
+	motor[back_left_drive_m] = speed;
+	motor[back_right_drive_m] = speed;
+}
+
+void strafeLeft(int speed)
+{
+	motor[front_left_drive_m] = -speed;
+	motor[front_right_drive_m] = -speed;
+	motor[back_left_drive_m] = -speed;
+	motor[back_right_drive_m] = -speed;
+}
+
+
+
+
 void middleAuto()	 // you can change these time values!!!
 {
-	motor[front_left_drive_m] = -95;						//strafe left for one second
-	motor[front_right_drive_m] = -95;						//max speed is 127, this is half speed currently
-	motor[back_left_drive_m] = -95;
-	motor[back_right_drive_m] = -95;
+	strafeLeft(95);
 	wait1Msec(1000);
-	motor[front_left_drive_m] = 0;							//stop
-	motor[front_right_drive_m] = 0;
-	motor[back_left_drive_m] = 0;
-	motor[back_right_drive_m] = 0;
+	strafeLeft(0);
 
 	// open claw .5s
 	updateClawMotors(127);
@@ -16,15 +45,9 @@ void middleAuto()	 // you can change these time values!!!
 	updateClawMotors(0);
 
 
-	motor[front_left_drive_m] = 95;							//drive forward for 1 sec
-	motor[front_right_drive_m] = -95;
-	motor[back_left_drive_m] = -95;
-	motor[back_right_drive_m] = 95;
+	driveForward(95);
 	wait1Msec(1000);
-	motor[front_left_drive_m] = 0;							//stop
-	motor[front_right_drive_m] = 0;
-	motor[back_left_drive_m] = 0;
-	motor[back_right_drive_m] = 0;
+	driveForward(0);
 
 
 	// close claw .5s
@@ -33,15 +56,9 @@ void middleAuto()	 // you can change these time values!!!
 	updateClawMotors(0);
 
 
-	motor[front_left_drive_m] = -95;							//drive reverse for 1 sec
-	motor[front_right_drive_m] = 95;
-	motor[back_left_drive_m] = 95;
-	motor[back_right_drive_m] = -95;
+	driveReverse(95);
 	wait1Msec(1000);
-	motor[front_left_drive_m] = 0;							//stop
-	motor[front_right_drive_m] = 0;
-	motor[back_left_drive_m] = 0;
-	motor[back_right_drive_m] = 0;
+	driveReverse(0);
 
 
 	//raise arm 1s
@@ -62,18 +79,10 @@ void middleAuto()	 // you can change these time values!!!
 	updateArmMotors(0);
 
 
-	motor[front_left_drive_m] = 95;							//drive forward for 1 sec
-	motor[front_right_drive_m] = -95;
-	motor[back_left_drive_m] = -95;
-	motor[back_right_drive_m] = 95;
+	driveForward(95);
 	wait1Msec(1000);
-	motor[front_left_drive_m] = 0;							//stop
-	motor[front_right_drive_m] = 0;
-	motor[back_left_drive_m] = 0;
-	motor[back_right_drive_m] = 0;
+	driveForward(0);
 }
-
-
 
 
 
@@ -91,19 +100,158 @@ void fwdAuto()
 	updateArmMotors(0);
 
 	//drive fwd 2.5s
-	motor[front_left_drive_m] = 95;
-	motor[front_right_drive_m] = -95;
-	motor[back_left_drive_m] = -95;
-	motor[back_right_drive_m] = 95;
+	driveForward(95);
 	wait1Msec(2500);
-	motor[front_left_drive_m] = 0;							//stop
-	motor[front_right_drive_m] = 0;
-	motor[back_left_drive_m] = 0;
-	motor[back_right_drive_m] = 0;
+	driveForward(0);
 }
 
 
+
+
+void rightAuto()
+{
+	// FACE ROBOT SIDEWAYS
+
+	updateClawMotors(127);			// open claw .5s
+	wait1Msec(500);
+	updateClawMotors(0);
+
+	updateArmMotors(127); 	//raise arm 1s
+	wait1Msec(1000);
+	updateArmMotors(0);
+
+	strafeLeft(95);						//strafe left	1.2 sec TO TILE
+	wait1Msec(1200);
+	strafeLeft(0);
+
+
+	driveForward(95);
+	wait1Msec(2000);
+	driveForward(0);
+
+	strafeLeft(95);						//strafe left TO FENCE
+	wait1Msec(775);
+	strafeLeft(0);
+
+
+	driveReverse(95);						//drive rev for 3 sec ALONG STARS
+	wait1Msec(3000);
+	driveReverse(0);
+
+}
+
+
+
+// There is currently a warning error for this function
+// (the yellow X) as the robot is not processing this code.
+// Only one autonomous is used at a time, so the other one
+// will have a warning error due to being unues at the time.
 void leftAuto()
+{
+	// open claw .5s
+	updateClawMotors(127);
+	wait1Msec(500);
+	updateClawMotors(0);
+
+	//raise arm 1s
+	updateArmMotors(127);
+	wait1Msec(1000);
+	updateArmMotors(0);
+
+
+	strafeRight(95);			   //strafe right	1.2 sec TO TILE
+	wait1Msec(1200);
+	strafeRight(0);
+
+	driveForward(95);					//drive forward for 2 sec TO WALL
+	wait1Msec(2000);
+	driveForward(0);
+
+
+	strafeRight(95);		  	//strafe right TO FENCE
+	wait1Msec(775);
+	strafeRight(0);
+
+
+	driveReverse(95);			  //drive rev for 3 sec
+	wait1Msec(3000);
+	driveReverse(0);
+}
+
+
+
+
+void totalLeftAuto() //ALL STARS, BEG AT LEFT SIDE
+{
+
+	strafeRight(100);				   //strafe right	1.2 sec TO TILE
+	wait1Msec(1200);
+	strafeRight(0);
+
+	updateClawMotors(-127);
+	wait1Msec(500);
+	updateClawMotors(0);
+
+	updateArmMotors(-127);
+	wait1Msec(700);
+	updateArmMotors(0);
+
+
+
+	driveForward(100);				//drive forward for 1.5 sec TO WALL
+	wait1Msec(2000);
+	driveForward(0);
+
+
+	strafeRight(120);			  	//strafe right TO FENCE
+	wait1Msec(900);
+	strafeRight(0);
+
+
+	driveReverse(100);			  //drive rev for 2 sec FOR LEFT STARS
+	wait1Msec(2000);
+	driveReverse(0);
+
+	strafeLeft(100);				   //strafe LEFT	.3 sec TO TILE, OFF CORNER
+	wait1Msec(300);
+	strafeLeft(0);
+
+	driveReverse(100);			  //drive rev for .5 sec PASS FOOT
+	wait1Msec(500);
+	driveReverse(0);
+
+
+	strafeRight(100);			  	//strafe right TO FENCE
+	wait1Msec(300);
+	strafeRight(0);
+
+
+	driveReverse(100);			  //drive rev for 2 sec FOR MIDDLE STARS
+	wait1Msec(2000);
+	driveReverse(0);
+
+
+	strafeLeft(100);				   //strafe LEFT	1 sec TO TILE, OFF CORNER
+	wait1Msec(300);
+	strafeLeft(0);
+
+	driveReverse(100);			  //drive rev for 1 sec PASS FOOT
+	wait1Msec(500);
+	driveReverse(0);
+
+	strafeRight(100);				   //strafe right	1 sec TO FENCE
+	wait1Msec(300);
+	strafeRight(0);
+
+	driveReverse(100);			  //drive rev for 3 sec FOR RIGHT STARS
+	wait1Msec(2000);
+	driveReverse(0);
+}
+
+
+
+
+void totalRightAuto()
 {
 	// open claw .5s
 	updateClawMotors(127);
@@ -149,74 +297,43 @@ void leftAuto()
 	motor[back_right_drive_m] = 0;
 
 
-	motor[front_left_drive_m] = -95;						//drive rev for 3 sec
+	motor[front_left_drive_m] = -95;						//drive rev for 7 sec
 	motor[front_right_drive_m] = 95;
 	motor[back_left_drive_m] = 95;
 	motor[back_right_drive_m] = -95;
-	wait1Msec(3000);
+	wait1Msec(7000);
 	motor[front_left_drive_m] = 0;							//stop
 	motor[front_right_drive_m] = 0;
 	motor[back_left_drive_m] = 0;
 	motor[back_right_drive_m] = 0;
-
 }
 
-// There is currently a warning error for this function
-// (the yellow X) as the robot is not processing this code.
-// Only one autonomous is used at a time, so the other one
-// will have a warning error due to being unues at the time.
-void rightAuto()
+
+void doLeft()
 {
-	// open claw .5s
-	updateClawMotors(127);
+	driveForward(127);
+	Wait1Msec(1000);
+	driveForward(0);
+
+	strafeLeft(95);
+	Wait1Msec(1000);
+	strafeLeft(0);
+
+	strafeRight(95);
+	Wait1Msec(500);
+	strafeRight(0);
+
+	updateClawMotors(-127);
 	wait1Msec(500);
 	updateClawMotors(0);
 
-	//raise arm 1s
-	updateArmMotors(127);
+		driveForward(127);
+	Wait1Msec(950);
+	driveForward(0);
+
+	updateArmMotors(-127);
 	wait1Msec(1000);
 	updateArmMotors(0);
 
 
-	motor[front_left_drive_m] = 85;				   //strafe right	1.2 sec
-	motor[front_right_drive_m] = 85;
-	motor[back_left_drive_m] = 85;
-	motor[back_right_drive_m] = 85;
-	wait1Msec(1200);
-	motor[front_left_drive_m] = 0;			   	//stop
-	motor[front_right_drive_m] = 0;
-	motor[back_left_drive_m] = 0;
-	motor[back_right_drive_m] = 0;
-
-	motor[front_left_drive_m] = 95;					//drive forward for 2 sec
-	motor[front_right_drive_m] = -95;
-	motor[back_left_drive_m] = -95;
-	motor[back_right_drive_m] = 95;
-	wait1Msec(2000);
-	motor[front_left_drive_m] = 0;					//stop driving
-	motor[front_right_drive_m] = 0;
-	motor[back_left_drive_m] = 0;
-	motor[back_right_drive_m] = 0;
-
-
-	motor[front_left_drive_m] = 95;			  	//strafe right
-	motor[front_right_drive_m] = 95;
-	motor[back_left_drive_m] = 95;
-	motor[back_right_drive_m] = 95;
-	wait1Msec(775);
-	motor[front_left_drive_m] = 0;			  	//stop
-	motor[front_right_drive_m] = 0;
-	motor[back_left_drive_m] = 0;
-	motor[back_right_drive_m] = 0;
-
-
-	motor[front_left_drive_m] = -95;			  //drive rev for 3 sec
-	motor[front_right_drive_m] = 95;
-	motor[back_left_drive_m] = 95;
-	motor[back_right_drive_m] = -95;
-	wait1Msec(3000);
-	motor[front_left_drive_m] = 0;					//stop driving
-	motor[front_right_drive_m] = 0;
-	motor[back_left_drive_m] = 0;
-	motor[back_right_drive_m] = 0;
 }
